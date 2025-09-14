12c9353dd4c01a008d2faf25d4bfdb37b06a4f7b
saeid <saeedmoradi@gmail.com>
2025-09-14 21:37:13 +0200

    add decode

--- caesaer-cipher.py
+++ caesaer-cipher.py
@@ -5,16 +5,25 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-
-
 def encrypt(plain_text, shift_amount):
   cipher_text = ""
   for letter in plain_text:
     position = alphabet.index(letter)
     new_position = position + shift_amount
-    new_letter = alphabet[new_position]
-    cipher_text += new_letter
+    cipher_text += alphabet[new_position]
   print(f"The encoded text is {cipher_text}")
 
 
-encrypt(plain_text=text, shift_amount=shift)
+def decrypt (cipher_text, shift_amount):
+  org_text = ""
+  for letter in cipher_text:
+    position = alphabet.index(letter)
+    new_position = position - shift_amount
+    org_text += alphabet[new_position]
+  print(f"The decoded text is {org_text}")
+
+
+if direction == "encode":
+  encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+  decrypt(cipher_text=text, shift_amount=shift)
